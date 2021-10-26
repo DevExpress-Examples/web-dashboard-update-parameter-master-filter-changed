34d18cd5581b41d58776c4b4cde9100ca781bc03
Sergey Nakhankov <sergey.nakhankov@devexpress.com>
2021-10-26 17:37:31 +0300

    Update VB.NET

--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -1,4 +1,5 @@
-﻿Imports DevExpress.DashboardCommon
+Imports Microsoft.VisualBasic
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DashboardWeb
 Imports System
 Imports System.Collections.Generic
@@ -8,26 +9,25 @@ Imports System.Web.UI
 Imports System.Web.UI.WebControls
 
 Partial Public Class _Default
-    Inherits System.Web.UI.Page
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+	End Sub
+	Protected Sub ASPxDashboard1_SetInitialDashboardState(ByVal sender As Object, ByVal e As SetInitialDashboardStateEventArgs)
+		e.InitialState = InitializeDashboardState()
+	End Sub
 
-    End Sub
-    Protected Sub ASPxDashboard1_SetInitialDashboardState(ByVal sender As Object, ByVal e As SetInitialDashboardStateEventArgs)
-        e.InitialState = InitializeDashboardState()
-    End Sub
+	Public Function InitializeDashboardState() As DashboardState
+		Dim dashboardState As New DashboardState()
 
-    Public Function InitializeDashboardState() As DashboardState
-        Dim dashboardState As New DashboardState()
+		Dim parameterState As New DashboardParameterState("OrderID", "10253", GetType(Integer))
 
-        Dim parameterState As New DashboardParameterState("OrderID", "10253", GetType(Integer))
+		Dim gridFilterState As New DashboardItemState("gridDashboardItem1")
+		gridFilterState.MasterFilterValues.Add(New Object() { 10253, "Hanari Carnes" })
 
-        Dim gridFilterState As New DashboardItemState("gridDashboardItem1")
-        gridFilterState.MasterFilterValues.Add(New Object() { 10253, "Hanari Carnes" })
-
-        dashboardState.Parameters.Add(parameterState)
-             dashboardState.Items.Add(gridFilterState)
-        Return dashboardState
-    End Function
+		dashboardState.Parameters.Add(parameterState)
+			 dashboardState.Items.Add(gridFilterState)
+		Return dashboardState
+	End Function
 
 End Class
